407512ba0fde9f9d79c48f8f94cb8916e5fc9854
keshav0710 <kavyatosh@gmail.com>
2026-04-12 01:26:09 +0530

    !

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Connected to Supabase Project: ksumvwsuirozoiwsiwp
-const supabaseUrl = 'https://ksumvwsuirozoiwsiwp.supabase.co';
+const supabaseUrl = 'https://ksumvwswiirozoiwsiwp.supabase.co';
 const supabaseAnonKey = 'sb_publishable_WXP2QNMEQB1kpyV7aSoPuw_LQno-HPO'; // New format publishable key
 const BUCKET_NAME = 'wedding-images'; // The name of the bucket we'll create
 
